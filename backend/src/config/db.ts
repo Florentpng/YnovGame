3bdbfe6ff3cf3b28a54aeb5ca1324765ec7a7b70
Flo <florent.jouinot@ynov.com>
2026-04-21 15:53:03 +0200

    Fin de l'architecture API

---
+++ backend/src/config/db.ts
@@ -0,0 +1,15 @@
+const mariadb = require('mariadb');
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 5,
+    queueLimit: 0
+});
+
+export default pool;
