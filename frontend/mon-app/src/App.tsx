3a05ca6e1ba9bd4665882ba0c11da6e918de909b
Flo <florent.jouinot@ynov.com>
2026-05-05 14:07:34 +0200

    commit before pull

--- frontend/mon-app/src/App.tsx
+++ frontend/mon-app/src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
-function Form({ title }: { title: string }) {
-  return <form>
+function Form({ title, isLoginMode }: { title: string; isLoginMode: boolean }) {
+  return <form onSubmit={isLoginMode ? login : register}>
     <h2>{title}</h2>
     <input type="text" name="username" placeholder="Username" required />
     <input type="password" name="password" placeholder="Password" required />
     <button type="submit">Valider</button>
   </form>
+}
+
+function login() {
+  
+}
+
+function register() {
+
 }
 
 function App() {
@@ -18,7 +26,7 @@ function App() {
     <>
       {!isLoggedIn ?
         <section id="login-section" >
-          {isLoginMode ? <Form title="Login" /> : <Form title="Register" />}
+          {isLoginMode ? <Form title="Login" isLoginMode={true} /> : <Form title="Register" isLoginMode={false} />}
         </section> : 
         <section id="game-section">
           
