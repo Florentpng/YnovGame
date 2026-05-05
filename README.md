280a635c38e87f734ad1bb63fccedcd06fe75419
Flo <florent.jouinot@ynov.com>
2026-05-05 09:19:39 +0200

    auth

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # YnovGame
-test
--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -7,8 +7,6 @@ authRouter.post('/login', async (req: Request, res: Response) => {
     let username = req.body.username
     let password = req.body.password;
 
-    console.log('Received login request with username:', username);
-
     try {
         const connection = await pool.getConnection();
 
@@ -17,7 +15,7 @@ authRouter.post('/login', async (req: Request, res: Response) => {
         if (users.length === 0) {
             res.status(401).send({ message: 'Invalid username or password' });
             return;
-        }
+        } 
 
         res.json(users);
     } catch (err) {
@@ -29,6 +27,25 @@ authRouter.post('/login', async (req: Request, res: Response) => {
 authRouter.post('/register', async (req: Request, res: Response) => {
     let username = req.body.username;
     let password = req.body.password;
+
+    try {
+        const connection = await pool.getConnection();
+
+        const users = await connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password]);
+
+        if (users.length > 0) {
+            res.status(400).send({ message: 'User already exists' });
+            return;
+        }
+
+        await connection.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password]);
+
+        res.status(201).send({ message: 'User registered successfully' });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: 'Error fetching users' });
+    }
 
 });
 
