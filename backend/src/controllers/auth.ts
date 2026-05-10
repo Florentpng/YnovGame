f414f604afa75d4d496eb53819e3006f199616e6
Ipo0912hub <hippolyte.bobe@ynov.com>
2026-05-10 20:37:50 +0200

    MDP chiffré: le football il a changé

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,59 +1,67 @@
 import { Router, Request, Response } from "express";
 import pool from "../config/db";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 const authRouter = Router();
 
-authRouter.post('/login', async (req: Request, res: Response) => {
-    console.log("Login request received with body:", req.body);
-    let username = req.body.username
-    let password = req.body.password;
+authRouter.post("/login", async (req: Request, res: Response) => {
+  let username = req.body.username;
 
-    try {
-        const connection = await pool.getConnection();
+  try {
+    const connection = await pool.getConnection();
 
-        const users = await connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password]);
+    const result = await connection.query("SELECT password FROM users WHERE username = ?",[username]);
+    const userPassword: Array<{ password: string }> = JSON.parse(JSON.stringify(result));
 
-        if (users) {
-            res.status(401).send({ message: 'identifiant ou mdp incorrect' });
-            return;
-        } 
-
-        res.json(users);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ message: 'Erreur avec un user' });
+    if (!userPassword.length) {
+      res.status(401).send({ message: "mdp incorrect" });
+      return;
     }
+
+    const isMatch = await bcrypt.compare(req.body.password, userPassword[0].password);
+    if (!isMatch) {
+      res.status(401).send({ message: "mdp incorrect" });
+      return;
+    }
+
+    res.json({username});
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Erreur avec un user" });
+  }
 });
 
-authRouter.post('/register', async (req: Request, res: Response) => {
-    let username = req.body.username;
-    let password = req.body.password;
+authRouter.post("/register", async (req: Request, res: Response) => {
+  let username = req.body.username;
+  let password = await bcrypt.hash(req.body.password, 10);
 
-    if (!username || !password) {
-        res.status(400).send({ message: 'Veuillez saisir un identifiant et un mdp valide'});
-        return;
+  if (!username || !password) {
+    res
+      .status(400)
+      .send({ message: "Veuillez saisir un identifiant et un mdp valide" });
+    return;
+  }
+
+  try {
+    const connection = await pool.getConnection();
+
+    const users = await connection.query("SELECT * FROM users WHERE username = ?",[username]);
+
+    if (users.length > 0) {
+      res.status(400).send({ message: "User déjà existant" });
+      return;
     }
 
-    try {
-        const connection = await pool.getConnection();
+    await connection.query(
+      "INSERT INTO users (username, password) VALUES (?, ?)",
+      [username, password],
+    );
 
-        const users = await connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password]);
-
-        if (users.length > 0) {
-            res.status(400).send({ message: 'User déjà existant' });
-            return;
-        }
-
-        await connection.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, password]);
-
-        res.status(201).send({ message: 'User bien enregistré' });
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).send({ message: 'Erreur avec un user' });
-    }
-
+    res.status(201).send({ message: "User bien enregistré" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Erreur avec un user" });
+  }
 });
 
 export default authRouter;
