61c58f2f7c710e42838a66bc2ee3feed077b45c7
Flo <florent.jouinot@ynov.com>
2026-06-30 10:00:59 +0200

    clean avant présentation

--- backend/dist/config/db.js
+++ backend/dist/config/db.js
@@ -3,15 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const mariadb = require('mariadb');
+const promise_1 = __importDefault(require("mysql2/promise"));
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const pool = mariadb.createPool({
+const path_1 = __importDefault(require("path"));
+dotenv_1.default.config({ path: path_1.default.join(__dirname, "../../.env") });
+const pool = promise_1.default.createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    connectionLimit: 5,
-    queueLimit: 0
 });
 exports.default = pool;
